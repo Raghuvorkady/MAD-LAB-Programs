11d3597355179ae16aa1a0bb94099cf987af2d0b
Raghuvorkady <raghavendrakm300@gmail.com>
2021-05-19 10:36:32 +0530

    added setOnCheckedChangeListener in p3

--- p3-LoginActivity/app/src/main/java/csmp/part_a/p3/MainActivity.java
+++ p3-LoginActivity/app/src/main/java/csmp/part_a/p3/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     EditText signUpUsernameField, signUpPasswordField;
     Button signUpButton;
@@ -30,49 +31,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pwdRuleSwitch = (SwitchCompat) findViewById(R.id.pwdRuleSwitch);
         pwdRuleTextView = (TextView) findViewById(R.id.pwdRuleTextView);
 
-        pwdRuleSwitch.setOnClickListener(this);
+        pwdRuleSwitch.setOnCheckedChangeListener(this);
         signUpButton.setOnClickListener(this);
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.pwdRuleSwitch:
-                if (pwdRuleSwitch.isChecked())
-                    pwdRuleTextView.setVisibility(View.VISIBLE);
-                else
-                    pwdRuleTextView.setVisibility(View.GONE);
-                break;
-            case R.id.signUpButton:
-                String username, password;
-                username = signUpUsernameField.getText().toString();
-                password = signUpPasswordField.getText().toString();
+        // no need of switch because, we are listening for a single button click
+        String username, password;
+        username = signUpUsernameField.getText().toString();
+        password = signUpPasswordField.getText().toString();
 
-                String pwdValidationRegex = "^(?=.*[0-9])"
-                        + "(?=.*[a-z])"
-                        + "(?=.*[A-Z])"
-                        + "(?=.*[@#$%^&+=])"
-                        + "(?=\\S+$).{8,}$";
-                //For ref: https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-expressions-in-java/
+        String pwdValidationRegex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])"
+                + "(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,}$";
+        //For ref: https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-expressions-in-java/
 
-                if (username.isEmpty())
-                    makeToast("Please don't forget to fill up username...");
-                if (!password.matches(pwdValidationRegex))
-                    makeToast("Password validation failed!");
-                else {
-                    Bundle userBundle = new Bundle();
-                    userBundle.putString(Constants.USERNAME_KEY, username);
-                    userBundle.putString(Constants.PASSWORD_KEY, password);
-                    Intent intent = new Intent(this, LoginActivity.class);
-                    intent.putExtras(userBundle);
-                    startActivity(intent);
-                }
-                break;
+        if (username.isEmpty())
+            makeToast("Please don't forget to fill up username...");
+        if (!password.matches(pwdValidationRegex))
+            makeToast("Password validation failed!");
+        else {
+            Bundle userBundle = new Bundle();
+            userBundle.putString(Constants.USERNAME_KEY, username);
+            userBundle.putString(Constants.PASSWORD_KEY, password);
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.putExtras(userBundle);
+            startActivity(intent);
         }
     }
 
     public void makeToast(String toastMessage) {
         Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if (isChecked)
+            pwdRuleTextView.setVisibility(View.VISIBLE);
+        else pwdRuleTextView.setVisibility(View.GONE);
     }
 }
