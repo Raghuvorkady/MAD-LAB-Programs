801ff0c4f7387222132fc1befdd154d535f8a012
Raghuvorkady <raghavendrakm300@gmail.com>
2021-06-23 09:31:02 +0530

    replaced buttons with toggleButton in p5

--- p5-CounterApp/app/src/main/java/csmp/part_a/p5/MainActivity.java
+++ p5-CounterApp/app/src/main/java/csmp/part_a/p5/MainActivity.java
@@ -1,66 +1,51 @@
 package csmp.part_a.p5;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    Button startButton, stopButton;
-    TextView counterValueText;
+    private TextView counterText;
 
-    int counter;
-    Handler handler;
-
-    Runnable runnable;
+    private int counter;
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startButton = (Button) findViewById(R.id.startButton);
-        stopButton = (Button) findViewById(R.id.stopButton);
-        counterValueText = (TextView) findViewById(R.id.counterValueTextView);
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        counterText = findViewById(R.id.counterTextView);
 
-        startButton.setOnClickListener(this);
-        stopButton.setOnClickListener(this);
+        toggleButton.setOnCheckedChangeListener(this);
 
         counter = 0;
         handler = new Handler();
     }
 
-    public void handleCounter() {
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if (isChecked)
+            handleCounter();
+        else handler.removeCallbacks(runnable);
+    }
+
+    private void handleCounter() {
         runnable = new Runnable() {
             @Override
             public void run() {
                 counter++;
-                counterValueText.setText(String.valueOf(counter));
+                counterText.setText(String.valueOf(counter));
                 handler.postDelayed(this, 100);
             }
         };
         handler.post(runnable);
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.startButton:
-                handleCounter();
-                startButton.setEnabled(false);
-                stopButton.setEnabled(true);
-                break;
-            case R.id.stopButton:
-                handler.removeCallbacks(runnable);
-                startButton.setEnabled(true);
-                stopButton.setEnabled(false);
-                break;
-        }
     }
 }
