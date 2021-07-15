198a634bfd1def65e8840ae771ad9ca2a238fe60
Raghuvorkady <raghavendrakm300@gmail.com>
2021-07-16 00:51:49 +0530

    modified p1 (removed unncessary)

---
+++ p1-VisitingCard/app/src/main/java/csmp/part_a/p1/MainActivity.java
@@ -0,0 +1,15 @@
+package csmp.part_a.p1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
